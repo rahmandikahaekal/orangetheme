870fe95ea43249aa09a0969087966b76c0bdfc00
rahmandika haekal <rahmandikahaekal31@gmail.com>
2017-03-17 16:17:01 +0700

    finishing work section and next progress created testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,35 @@ $(function () {
        
  //  }  
 // });
+
+$('.main_work').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+});
+
+// Isotope click function
+$('.work_nav ul li').click(function() {
+    $('.work_nav ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.main_work').isotope({
+        filter: selector
+    });
+    return false;
+});
+
+// Magnific Popup
+    $('.portfolio-popup').magnificPopup({
+        type: 'image',
+          gallery: {
+              enabled: true
+          },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+              return element.find('img');
+            }
+        }
+    });
